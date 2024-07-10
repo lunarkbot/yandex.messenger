fdb2f5def83e2249fa53d4dea9af5738428db1d7
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-07-10 15:08:06 +0200

    Implemented initial project setup with TypeScript and Vite support
    - Initialized the project using Vite and TypeScript.
    - Added key dependencies: express for the server, autoprefixer, and path.
    - Set up static file serving through an express server.
    - Configured build and start scripts in `package.json`.

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__DIRNAME, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__DIRNAME, 'dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
+});
