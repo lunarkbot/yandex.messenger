8a0b84a2db9958b6245bda70163a21a00fb8fcd6
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-07-11 22:20:49 +0200

    Remove redundant files from the project.
    This commit removes unnecessary files that were no longer used in the project.

--- src/main.tmpl.js
+++
@@ -1,7 +0,0 @@
-const template =
-  `
-    <div class="{{nameClass}}">Hello, {{name}} </div>
-    <div>Hello, {{name}} </div>
-    <div>Hello, {{name}} </div>
-    <div>Hello, {{name}} </div>
-  `;
--- src/utils/renderTemplate.js
+++
@@ -1,9 +0,0 @@
-/**
- * Render a precompiled template with the given context.
- * @param precompiledFunction {function} - precompiled template function
- * @param context {object} - context for the template
- * @returns {string} - rendered template
- */
-export default function renderTemplate(precompiledFunction, context) {
-  return precompiledFunction(context);
-}
