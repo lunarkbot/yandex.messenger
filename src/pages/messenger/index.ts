cc91f49fe2d7ed2fbed2f1d1380b219e2949830b
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-07-12 12:51:06 +0200

    Add messenger page

--- src/pages/messenger/index.ts
+++ src/pages/messenger/index.ts
@@ -1,14 +1,5 @@
 import renderMessenger from './messenger.tmpl.js';
-import styles from './messenger.module.css';
-import { Template } from '../../types';
 
-const context:Template = {
-  headingClassName: styles.heading,
-  messageClassName: styles.message,
-  linkClassName: styles.link,
-  pageClassName: styles.page,
-}
-
-const messenger:string = renderMessenger(context);
+const messenger:string = renderMessenger();
 
 export default messenger;
