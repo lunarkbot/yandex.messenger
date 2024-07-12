06beb02b56b2b26a502c9fd67b513e6b10de8420
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-07-12 12:50:14 +0200

    Add messenger page

--- src/pages/messenger/messenger.tmpl.js
+++ src/pages/messenger/messenger.tmpl.js
@@ -1,5 +1,14 @@
 import styles from './messenger.module.css';
 
 const template = `
-  
+  <div class="${styles.messenger}">
+    <aside class="${styles.sidebar}">
+      <ul class="${styles.contactList}">
+        <li class="${styles.contactListItem}">Андрей</li>
+        <li class="${styles.contactListItem}">Василий</li>
+        <li class="${styles.contactListItem}">Петр</li>
+      </ul>
+    </aside>
+    <div class="${styles.main}"></div>
+  </div>
 `;
