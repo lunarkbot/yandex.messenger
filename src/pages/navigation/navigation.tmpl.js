1352bcb22b1c37e1e786d93866f844131753ea7f
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-07-13 11:27:18 +0200

    fix: use the semantically correct nav tag in navigation

--- src/pages/navigation/navigation.tmpl.js
+++ src/pages/navigation/navigation.tmpl.js
@@ -1,26 +1,28 @@
 import styles from './navigation.module.css';
 
-const template = `
-<div class="${styles.page}">
-  <ul class="${styles.links}">
-    <li>
-      <a class="${styles.link}" href="/signUp">Registation</a>
-    </li>
-    <li>
-      <a class="${styles.link}" href="/signIn">Login</a>
-    </li>
-    <li>
-      <a class="${styles.link}" href="/profile">Profile</a>
-     </li>
-    <li>
-      <a class="${styles.link}" href="/messenger">Messenger</a>
-    </li>
-    <li>
-     <a class="${styles.link}" href="/notFound">Error 404</a>
-    </li>
-    <li>
-     <a class="${styles.link}" href="/serverError">Error 500</a>
-    </li>
-  </ul>
-</div>
-`;
+export default function (context) {
+  return `
+    <nav class="${styles.page}">
+      <ul class="${styles.links}">
+        <li>
+          <a class="${styles.link}" href="/signUp">Registation</a>
+        </li>
+        <li>
+          <a class="${styles.link}" href="/signIn">Login</a>
+        </li>
+        <li>
+          <a class="${styles.link}" href="/profile">Profile</a>
+         </li>
+        <li>
+          <a class="${styles.link}" href="/messenger">Messenger</a>
+        </li>
+        <li>
+         <a class="${styles.link}" href="/notFound">Error 404</a>
+        </li>
+        <li>
+         <a class="${styles.link}" href="/serverError">Error 500</a>
+        </li>
+      </ul>
+    </nav>
+  `;
+}
