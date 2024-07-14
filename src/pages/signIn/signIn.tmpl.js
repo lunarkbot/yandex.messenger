705136047548f81071c54e8c949bbddd722d9c94
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-07-14 08:19:57 +0200

    add form tags to authorization and registration templates

--- src/pages/signIn/signIn.tmpl.js
+++ src/pages/signIn/signIn.tmpl.js
@@ -3,14 +3,16 @@ import styles from './signIn.module.css';
 export default function (context) {
   return `
     <div class="${styles.page}">
-      <h1 class="${styles.heading}">Вход</h1>
-      <div class="${styles.inputGroup}">
-        {{loginInput}}
-        {{passwordInput}}
-      </div>
-      
-      {{signInButton}}
-      <a href="/signUp" class="${styles.link}">Нет аккаунта?</a>
+      <form name="signin">
+        <h1 class="${styles.heading}">Вход</h1>
+        <div class="${styles.inputGroup}">
+          {{loginInput}}
+          {{passwordInput}}
+        </div>
+        
+        {{signInButton}}
+        <a href="/signUp" class="${styles.link}">Нет аккаунта?</a>
+      </form>
     </div>
   `;
 }
--- src/pages/signUp/signUp.tmpl.js
+++ src/pages/signUp/signUp.tmpl.js
@@ -3,19 +3,21 @@ import styles from './signUp.module.css';
 export default function (context) {
   return `
     <div class="${styles.page}">
-      <h1 class="${styles.heading}">Регистрация</h1>
-      <div class="${styles.inputGroup}">
-        {{emailInput}}
-        {{loginInput}}
-        {{firstNameInput}}
-        {{secondNameInput}}
-        {{phoneInput}}
-        {{passwordInput}}
-        {{passwordCheckInput}}
-      </div>
-      
-      {{signUpButton}}
-      <a href="/signIn" class="${styles.link}">Войти</a>
+      <form name="signup">
+        <h1 class="${styles.heading}">Регистрация</h1>
+        <div class="${styles.inputGroup}">
+          {{emailInput}}
+          {{loginInput}}
+          {{firstNameInput}}
+          {{secondNameInput}}
+          {{phoneInput}}
+          {{passwordInput}}
+          {{passwordCheckInput}}
+        </div>
+        
+        {{signUpButton}}
+        <a href="/signIn" class="${styles.link}">Войти</a>
+      </form>
     </div>
   `;
 }
